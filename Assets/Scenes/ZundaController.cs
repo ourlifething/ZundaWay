859d6ec14366fe6a43eedf974c3ca78a6ef39381
taku <rx77kz@docomo.ne.jp>
2023-09-25 14:34:17 +0900

    ずんだ餅つくった

---
+++ Assets/Scenes/ZundaController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ZundaController : MonoBehaviour
+{
+    float fallspeed;
+    void Start()
+    {
+        this.fallspeed = 0.01f + 0.1f * Random.value;
+    }
+    void Update()
+    {
+        transform.Translate(0, -fallspeed, 0);
+        
+        if (transform.position.y < -5.5f){
+            Destroy (gameObject);
+        }
+    }
+}
