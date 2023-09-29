1cae3324e3f9818507fe45ef3d34de7dcbcfbc2f
taku <rx77kz@docomo.ne.jp>
2023-09-29 14:51:08 +0900

    ゲームオーバー処理、スクロール処理

--- Assets/Scripts/Takusari/GameController.cs
+++ Assets/Scripts/Takusari/GameController.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -16,10 +19,20 @@ public class GameController : MonoBehaviour
     public MondazunController mondazun;
     public ZundaGenerator generator;
     public ZundaGenerator ankoGene;
+    public UIController ucon;
+    public ScrollScript scSp;
+
     public int normaScore;
+    public Text normaText;
+    public float count;
+    public Text countText;
+    public Text startText;
+    public Text gameOverText;
     void Start()
     {
         Ready();
+        gameOverText.gameObject.SetActive(false);
+        startText.gameObject.SetActive(false);
     }
 
     void LateUpdate()
@@ -30,7 +43,12 @@ public class GameController : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.Space)) GameStart();
                 break;
             case State.Play:
-                if (mondazun.Miss()) GameOver();
+                startText.text = "Start!";
+                Invoke(nameof(DesText),1f);
+                Destroy(normaText);
+                count -= Time.deltaTime;
+                countText.text = "あと" + count.ToString("f1") + "m";
+                if (mondazun.Miss() || count <= 0) GameOver();
                 break;
             case State.GameOver:
                 if (Input.GetKeyDown(KeyCode.Space)) Reload();
@@ -41,6 +59,8 @@ public class GameController : MonoBehaviour
     void Ready()
     {
         state = State.Ready;
+        normaText.text = normaScore.ToString() + "個集めよう！";
+
         mondazun.SetSteerActive(false);
         generator.geneStop();
         ankoGene.geneStop();
@@ -57,6 +77,20 @@ public class GameController : MonoBehaviour
     void GameOver()
     {
         state = State.GameOver;
+        int thisScore = ucon.getScore();
+        if (mondazun.Miss())
+        {
+            gameOverText.text = "ゲームオーバー...";
+        }
+        if (count <= 0 && thisScore < normaScore)
+        {
+            gameOverText.text = "ノルマ未達成...\nあと" + (normaScore - thisScore) + "個";
+        }
+        if (count <= 0 && thisScore >= normaScore)
+        {
+            gameOverText.text = "クリア!";
+        }
+        gameOverText.gameObject.SetActive(true);
 
         mondazun.SetSteerActive(false);
         generator.geneStop();
@@ -74,7 +108,12 @@ public class GameController : MonoBehaviour
     }
     void Reload()
     {
+        gameOverText.gameObject.SetActive(false);
         string currentSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(currentSceneName);
+    }
+    void DesText()
+    {
+        startText.gameObject.SetActive(false);
     }
 }
