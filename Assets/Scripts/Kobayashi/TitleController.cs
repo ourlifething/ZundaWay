be7a6a47f0949d35d35032fb9e3b91f01e3efbdd
okuda <okudaharuka12@gmail.com>
2023-10-04 14:16:56 +0900

    voice追加

--- Assets/Scripts/Kobayashi/TitleController.cs
+++ Assets/Scripts/Kobayashi/TitleController.cs
@@ -4,14 +4,27 @@ using DG.Tweening;
 using Microsoft.Unity.VisualStudio.Editor;
 
 public class TitleController : MonoBehaviour
-{ 
+{
+    AudioSource[] audioTitle;
 
     // Start is called before the first frame update
-     void Start()
+    void Start()
     {
-        transform.DOLocalMove(new Vector3(-0.03f, 2.63f, 0f), 3f)
-        .SetEase(Ease.InOutElastic);
+        audioTitle = GetComponents<AudioSource>();
+        // 落下音再生タイミング
+        Invoke("PlayAudioAfterDelay", 0.8f);
 
+        //落下アニメーション
+        transform.DOLocalMove(new Vector3(-0.03f, 2.63f, 0f), 2.5f)
+            .SetEase(Ease.InOutElastic)
+            .OnComplete(() =>
+            {
+                 audioTitle[1].Play();
+            });     
+    }
+    void PlayAudioAfterDelay()
+    {
+        audioTitle[0].Play();
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Okuda/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SoundManager : MonoBehaviour
+
+{
+    AudioSource[] audioStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioStart = GetComponents<AudioSource>();
+        audioStart[0].Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
