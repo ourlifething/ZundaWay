3b79645603814253d2c3a52e3860bfbd8d851386
okuda <okudaharuka12@gmail.com>
2023-10-11 10:25:43 +0900

    pause設定

--- Assets/Scripts/Okuda/StartAsobikataButton.cs
+++ Assets/Scripts/Okuda/StartAsobikataButton.cs
@@ -6,6 +6,7 @@ using static UnityEngine.Windows.WebCam.VideoCapture;
 public class StartAsobikataButton : MonoBehaviour
 {
     AudioSource[] audioStartVoice;
+    public GameObject SMObject;
     void Start()
     {
         audioStartVoice = GetComponents<AudioSource>();
@@ -13,10 +14,14 @@ public class StartAsobikataButton : MonoBehaviour
     public void OnClickAsobikata()
     {
         audioStartVoice[0].Play();
+        AudioSource[] pauseAudio = SMObject.GetComponents<AudioSource>();
+        pauseAudio[1].Pause();
     }
     public void OnClickStart()
     {
         audioStartVoice[0].Play();
+        AudioSource[] pauseAudio = SMObject.GetComponents<AudioSource>();
+        pauseAudio[1].Pause();
     }
     
 }
