e116e7934d74bfca4ac009bd8346f70a3ff8fe7c
yuquikoba <kobayukapc@gmail.com>
2023-10-12 18:16:40 +0900

    ランキング%の追加

--- Assets/Scripts/Kobayashi/ScoreController.cs
+++ Assets/Scripts/Kobayashi/ScoreController.cs
@@ -12,6 +12,8 @@ public class ScoreController : MonoBehaviour
     public static int score3;
     public static int scoreTotal;
     public int rank;
+    public float id;
+    public float rankIn;
     
     public RectTransform popup;
     public Image image;
@@ -44,8 +46,12 @@ public class ScoreController : MonoBehaviour
         }else{
             var ranking=JsonUtility.FromJson<RankingData>(uwr.downloadHandler.text);
             Debug.Log(ranking.lastId+":"+ranking.rank);
+            Debug.Log(ranking.rankIn);
 
             rank=ranking.rank;
+            id=ranking.lastId;
+            rankIn=ranking.rankIn;
+            Debug.Log(rank+" "+id+" "+rankIn);
 
             Invoke(nameof(Popup),3f);
 
@@ -58,8 +64,8 @@ public class ScoreController : MonoBehaviour
             image.DOFade(1,1f);
             popup.DOScale(new Vector3(3,3,3),1f);
             popup.DOPunchPosition(new Vector3(0,2,0),1f); 
-
-            text.DOText("総合獲得ずんだ数は...\n"+scoreTotal+"個!\n\nランキングは"+rank+"位です!",4f);
+            
+            text.DOText("総合獲得ずんだ餅は...\n"+scoreTotal+"個!\nランキングは"+rank+"位\n上位"+Math.Round(rankIn/id*100)+"%の実力です!",4f);
 
     }
 
@@ -68,4 +74,5 @@ public class ScoreController : MonoBehaviour
 class RankingData{
     public int lastId;
     public int rank;
+    public int rankIn;
 }
