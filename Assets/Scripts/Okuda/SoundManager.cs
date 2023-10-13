224c158aa5a27ba943300dd9374b7d9eebd53e3e
okuda <okudaharuka12@gmail.com>
2023-10-13 10:45:43 +0900

    オーディオスクリプト修正

--- Assets/Scripts/Okuda/SoundManager.cs
+++ Assets/Scripts/Okuda/SoundManager.cs
@@ -7,6 +7,11 @@ public class SoundManager : MonoBehaviour
 
 {
     AudioSource[] audioStart;
+    //スタートボタンがクリックされている状態かどうか
+    private bool StartButtonClicked = false;
+    //遊び方ボタンがクリックされている状態かどうか
+    private bool AsobikataButtonClicked = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +22,17 @@ public class SoundManager : MonoBehaviour
         Invoke("PlayAudioAfterDelay", 0.8f);
 
         //タイトルコール開始
-        Invoke("VoiceTitleCall", 2.8f);
+        if(StartButtonClicked == false && AsobikataButtonClicked == false)
+        {
+            Invoke("VoiceTitleCall", 2.8f);
+        }
+        
 
         // ゲーム説明音声開始
-        Invoke("VoiceGameDescription", 5.2f);
-
+        if (StartButtonClicked == false && AsobikataButtonClicked == false)
+        {
+            Invoke("VoiceGameDescription", 5.2f);
+        }
     }
     //落下音
     void PlayAudioAfterDelay()
@@ -41,19 +52,29 @@ public class SoundManager : MonoBehaviour
     //遊び方ボタンがクリックされたら
     public void OnClickAsobikata()
     {
+        AsobikataButtonClicked = true;
         audioStart[4].Play();
         //クリックされたらゲーム説明を中止
         audioStart[1].Pause();
         //クリックされたらタイトルコールを中止
         audioStart[3].Pause();
+
+        // 遊び方ボタンがクリックされたら、ゲームの説明 or タイトルコールをキャンセル
+        CancelInvoke("VoiceGameDescription");
+        CancelInvoke("VoiceTitleCall");
     }
     //スタートボタンがクリックされたら
     public void OnClickStart()
     {
+        StartButtonClicked = true;
         audioStart[5].Play();
         //クリックされたらゲーム説明を中止
         audioStart[1].Pause();
         //クリックされたらタイトルコールを中止
         audioStart[3].Pause();
+
+        // スタートボタンがクリックされたら、ゲームの説明 or タイトルコールをキャンセル
+        CancelInvoke("VoiceGameDescription");
+        CancelInvoke("VoiceTitleCall");
     }
 }
