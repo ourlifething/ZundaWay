b603b07698057baaf6e1e833025674b0ac7761d4
yuquikoba <kobayukapc@gmail.com>
2023-10-13 13:04:18 +0900

    イメージ追加

--- Assets/Scripts/Kobayashi/TitleController.cs
+++ Assets/Scripts/Kobayashi/TitleController.cs
@@ -5,12 +5,14 @@ using Microsoft.Unity.VisualStudio.Editor;
 
 public class TitleController : MonoBehaviour
 {
+    public GameObject logo;
+    public GameObject logoBack;
     // Start is called before the first frame update
     void Start()
     {
-        //落下アニメーション
-        transform.DOLocalMove(new Vector3(-0.03f, 2.63f, 0f), 2.5f)
+        logo.transform.DOLocalMove(new Vector3(-0.03f, 2.63f, 0f), 2.5f)
             .SetEase(Ease.InOutElastic);     
+        logoBack.transform.DOLocalMove(new Vector3(-0.13f, 2.53f, 0f), 2.5f);
     }
     
 
