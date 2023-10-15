d49e15c60b34018559ac9fa063986b86a42831d8
okuda <okudaharuka12@gmail.com>
2023-10-15 23:12:24 +0900

    画面遷移BGMマネージャー追加

--- Assets/Scripts/Okuda/BgmManager.cs
+++ Assets/Scripts/Okuda/BgmManager.cs
@@ -1,27 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static UnityEngine.Windows.WebCam.VideoCapture;
 
 public class BgmManager : MonoBehaviour
 {
-    AudioSource audioBgm;
-    public bool DontDestroyEnabled = true;
-    // Start is called before the first frame update
-    void Start()
+    //スタートからHowToPlayへの画面遷移はBGMが変わらずなり続ける仕様
+    public static BgmManager instance;//static 変数でBgmManager型のインスタンスを保持
+    void Awake()
     {
-        if (DontDestroyEnabled)
+        //初回のAwakeの時のみここがtrueになりインスタンスが登録される
+        if (instance == null)
         {
-            DontDestroyOnLoad(this);
+            instance = this;//このインスタンスをstatic な instanceに登録
+            DontDestroyOnLoad(gameObject);
         }
-        audioBgm = GetComponent<AudioSource>();
-        audioBgm.Play();
-
+        else
+        {
+            Destroy(gameObject);//２回目以降重複して作成してしまったgameObjectを削除
+        }
     }
+    
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+       
+    }
+    // HowToPlay以外のシーンに遷移した場合はBGMを停止
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name != "HowToPlay")
+        {
+            Destroy(gameObject);
+        }
     }
 }
